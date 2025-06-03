ba12b723774e56e80d0c51d5e2070580b207bc7f
Virendrachavan11 <chavanvirendra11@gmail.com>
2025-06-04 00:12:09 +0530

    Update vite.config.js

--- exam-portal/vite.config.js
+++ exam-portal/vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const VITE_BACKEND_URL = "https://exam-portal-backend-hvq6.onrender.com"
-
-const BACKEND_URL =
-  import.meta.env.PROD && VITE_BACKEND_URL
-    ? import.meta.env.VITE_BACKEND_URL
-    : "http://localhost:3000";
-
-export default BACKEND_URL;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
