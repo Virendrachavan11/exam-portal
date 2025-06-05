50f98a28e15071abc1807bd6093ae6b6b36d0a02
Virendra <chavanvirendra11@gmail.com>
2025-06-05 22:56:56 +0530

    add code of replace img url from backend 3a try1

--- exam-backend/main.js
+++ exam-backend/main.js
@@ -53,15 +53,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: "*",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+
+
+
+
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
