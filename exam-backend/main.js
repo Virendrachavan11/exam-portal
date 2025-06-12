943659ec25faf028bb89c1004eab840b24bd9159
Virendra <chavanvirendra11@gmail.com>
2025-06-12 14:45:27 +0530

    Min backend fix

--- exam-backend/main.js
+++ exam-backend/main.js
@@ -22,7 +22,6 @@ import CandExamRouter from "./routers/candidateRouters/CandExamRouter.js"
 
 import ManageSv from "./routers/superAdminRoutes/ManageSv_Routers.js"
 
-import convertDatesMiddleware from "./middleware/convertDatesToIST.js";
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
